0e66152880fbb2c84b5ccb1e322a7e305e3e3270
leng128888 <1203246179@qq.com>
2025-11-10 13:26:08 +0800

    Delete Ikuu_getcookie.js

--- Ikuu_getcookie.js
+++
@@ -1,9 +0,0 @@
-// ikuuu_getcookie.js
-if ($request.headers) {
-  const cookie = $request.headers['Cookie'];
-  if (cookie) {
-    $persistentStore.write(cookie, "ikuuu_cookie");
-    $notification.post("🍪 Cookie 获取成功", "iKuuu", "已保存 Cookie");
-  }
-}
-$done({});
